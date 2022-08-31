d14f63b527f94feeea58b2fa24af037db2a1ad9e
Fernando Cavalcanti <comododk@gmail.com>
2022-08-30 23:29:25 -0300

    feat: db migrate photos table

--- backend/src/routes/fotoRoutes.js
+++ backend/src/routes/fotoRoutes.js
@@ -1,8 +1,9 @@
 ﻿import { Router } from 'express';
 import fotoController from '../controllers/FotoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
